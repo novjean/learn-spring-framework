3b0ca95b90242f484d47b00c9e52044b8e286cd9
Novjean J Kannathara <novjeanj@gmail.com>
2024-04-07 12:06:38 +0530

    gaming console interface

--- src/main/java/com/novatech/learnspringframework/game/MarioGame.java
+++ src/main/java/com/novatech/learnspringframework/game/MarioGame.java
@@ -1,6 +1,6 @@
 package com.novatech.learnspringframework.game;
 
-public class MarioGame {
+public class MarioGame implements GamingConsole {
 	public void up() {
 		System.out.println("Jump");
 	}
--- src/main/java/com/novatech/learnspringframework/game/PacmanGame.java
+++ src/main/java/com/novatech/learnspringframework/game/PacmanGame.java
@@ -1,19 +1,17 @@
 package com.novatech.learnspringframework.game;
 
-public class SuperContraGame implements GamingConsole{
-	
+public class PacmanGame implements GamingConsole {
 	public void up() {
 		System.out.println("up");
 	}
 	
 	public void down() {
-		System.out.println("sit down");
+		System.out.println("down");
 	}
 	public void left() {
-		System.out.println("Go back");
+		System.out.println("left");
 	}
 	public void right() {
-		System.out.println("Shoot a bullet");
+		System.out.println("right");
 	}
-
 }
