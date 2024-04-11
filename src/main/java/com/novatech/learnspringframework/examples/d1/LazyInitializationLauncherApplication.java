69dd6890e7ba454a04a10ffc609b3ee586564cef
Novjean J Kannathara <novjeanj@gmail.com>
2024-04-11 10:53:49 +0530

    lazy, bean scope, preDestroy, inject

---
+++ src/main/java/com/novatech/learnspringframework/examples/d1/LazyInitializationLauncherApplication.java
@@ -0,0 +1,54 @@
+package com.novatech.learnspringframework.examples.d1;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+@Component
+class ClassA{
+	
+}
+
+@Component
+@Lazy
+// when lazy is added the initialization of classB bean does not happen on startup
+// only when the classB bean is being used will it get initialized
+class ClassB{
+	private ClassA classA;
+	
+	public ClassB(ClassA classA) {
+		System.out.println("some initialization logic");
+		this.classA = classA;
+	}
+	
+	public void doSomething() {
+		System.out.println("do something in here");
+	}
+	
+}
+
+@Configuration
+@ComponentScan
+public class LazyInitializationLauncherApplication {
+	
+	public static void main(String[] args) {
+		//try with resource block
+		try(var context = new AnnotationConfigApplicationContext(LazyInitializationLauncherApplication.class)){
+			//Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+			
+			System.out.println("initialization of context is completed");
+			
+			context.getBean(ClassB.class).doSomething();
+		}
+		
+	}
+
+}
+
+
+
+
---
+++ src/main/java/com/novatech/learnspringframework/examples/g1/CdiContextLauncherApplication.java
@@ -0,0 +1,55 @@
+package com.novatech.learnspringframework.examples.g1;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+
+//@Component
+@Named
+class BusinessService {
+	private DataService dataService;
+	
+	
+	public DataService getDataService() {
+		return dataService;
+	}
+	
+	//@Autowired
+	@Inject
+	public void setDataService(DataService dataService) {
+		System.out.println("setter injection");
+		this.dataService = dataService;
+	}
+}
+
+@Named
+class DataService {
+	
+}
+
+@Configuration
+@ComponentScan
+public class CdiContextLauncherApplication {
+	
+	public static void main(String[] args) {
+		//try with resource block
+		try(var context = new AnnotationConfigApplicationContext(CdiContextLauncherApplication.class)){
+			Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+			
+			System.out.println(context.getBean(BusinessService.class).getDataService());
+		}
+		
+	}
+
+}
+
+
+
+
